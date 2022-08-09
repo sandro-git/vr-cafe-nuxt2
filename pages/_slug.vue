e6b3c7e610919c7e1a6fd1ee24213d3accf0b7fa
sandro <sandroazerty@icloud.com>
2022-08-09 12:45:52 +0200

    Revert "solve slug request issue"
    This reverts commit 86348cb0ab09974e5cff77e72fd4b6c65c78761e.

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="flex flex-col items-center">
+    <h1 class="text-4xl font-bold">
+      <!-- {{ game.name }} Page -->
+    </h1>
     <nuxt-link to="/" class="bg-gray-400 text-white text-xl px-4 py-2 rounded-xl">
       Home Page
     </nuxt-link>
-    <pre>{{ game }}</pre>
+    {{ $route.params.slug }}
+    >>>>>>> parent of 86348cb (solve slug request issue)
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'SlugPage',
-  layout: 'gameLayout',
-  data () {
-    return {
-      game: ''
-    }
-  },
-  async fetch (params) {
-    this.game = await fetch(`https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[slug.current == "${params.slug}"]{name,text,'imageUrl':image{asset},slug{current},players,difficulty,youtube}`).then(res => res.json())
-  }
-}
 
+  name: 'SlugPage'
+}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,12 @@
 <template>
-  <div class="flex flex-col items-center gap-5">
-    <LandingImage />
-    <div v-for="game in computedGames" :key="game._id" class="flex flex-col items-center ">
-      <h1 class="text-4xl font-bold">
-        {{ game.name }}
-      </h1>
-      <nuxt-link :to="game.slug.current">
-        {{ game.slug.current }}
-      </nuxt-link>
-      <SanityImage
-        :asset-id="game.imageUrl.asset._ref"
-        auto="format"
-        height="250"
-        witdh="450"
-        style="height: 250px; width: 450px;"
-      />
+  <div class="flex flex-col items-center">
+    <h1 class="text-4xl font-bold">
+      Home Page
+    </h1>
+    <Button text="About" :path="path" />
+    <div v-for="game in games.result" :key="game._id" class="flex flex-col">
+      <Button :text="game.name" />
+      <p>{{ game.slug.current }}</p>
     </div>
   </div>
 </template>
@@ -23,17 +15,20 @@
 import LandingImage from '~/components/landingImage.vue'
 export default {
   name: 'IndexPage',
-  components: { LandingImage },
+  async asyncData ({ $axios }) {
+    const games = await $axios.$get('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "game"]{name,_id,slug{current}}')
+    return { games }
+  },
   data () {
     return {
-      games: null
+      path: { name: 'about' },
+      gamePath: { name: '', params: { user: '1232' } }
     }
   },
-  async fetch () {
-    this.games = await this.$axios.$get('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "game"]{name,text,"imageUrl": image{asset},editor->{name},slug{current}}')
-  },
   computed: {
-    computedGames: () => this.games.result
+    gameProp () {
+      return { name: '', params: this.games }
+    }
   }
 }
 </script>
