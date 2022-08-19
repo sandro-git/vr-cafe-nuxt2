609293f096274adffdcf13b04a8bffe40c1ccfa4
zozo404 <enzocuny66@gmail.com>
2022-08-19 12:23:12 +0200

    🚨 header

--- components/Header.vue
+++ components/Header.vue
@@ -1,66 +1,72 @@
 <template>
   <header class="bg-gray-900 lg:h-40 w-full z-20">
     <nav
-      class="bg-gray-900 px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0"
+      class="lg:max-h-40 w-full py-6 grid grid-cols-2 grid-rows-2 lg:grid-rows-1 lg:grid lg:grid-cols-3 items-center justify-between lg:border-none bg-gray-900"
       aria-label="Top"
     >
-      <div class="flex flex-wrap justify-between md:justify-center xl:justify-between items-center">
-        <!-- logo -->
-        <a href="/" class="flex justify-center lg:col-span-1 lg:h-full h-2/3">
-          <span class="sr-only">Logo</span>
-          <nuxt-img
-            provider="sanity"
-            :src="logo.imageId.asset._ref"
-            alt="logo vr café"
-            class="h-full lg:w-44 w-20"
-            sizes="xs:100vw"
-          />
-        </a>
-        <!-- bouton réserver -->
-        <div class="flex md:order-2">
-          <a
-            href="reservation-simply.html"
-            class="md:inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base lg:text-2xl font-medium text-indigo-700 hover:bg-indigo-50 hidden"
-          >Réserver</a>
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
-            @click="toggleNavbar()"
+      <!-- logo -->
+      <a href="/" class="flex justify-center lg:col-span-1 lg:h-full h-2/3">
+        <span class="sr-only">Logo</span>
+        <nuxt-img
+          provider="sanity"
+          :src="logo.imageId.asset._ref"
+          alt="logo vr café"
+          class="h-full"
+          sizes="xs:100vw"
+        />
+      </a>
+      <!-- bouton réserver -->
+      <div class="flex justify-center lg:col-start-3">
+        <a
+          href="/"
+          class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base lg:text-2xl font-medium text-indigo-700 hover:bg-indigo-50"
+        >Réserver</a>
+      </div>
+      <!-- navigation mobile -->
+      <div
+        class="col-span-2 lg:col-span-1 lg:col-start-2 lg:row-start-1 flex flex-col items-center"
+      >
+        <div class="mb-4 py-4 flex space-x-6">
+          <!-- acceuil -->
+          <nuxt-link
+            key="Solutions"
+            to="/"
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
           >
-            <span class="sr-only">Open menu</span>
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" /></svg>
-          </button>
-        </div>
-        <!-- navigation mobile -->
-        <div id="navbar-sticky" :class="{'hidden': !showMenu, 'flex': showMenu}" class="md:justify-between justify-center items-center w-full md:flex md:w-auto md:order-1 bg-gray-900">
-          <div class="flex flex-col p-4 mt-4 rounded-lg borde md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
-            <nuxt-link key="Solutions" to="/" class="block py-2 pr-4 pl-3 text-base lg:text-2xl font-medium text-white hover:text-indigo-50" aria-current="page">
-              Accueil
-            </nuxt-link>
-
-            <nuxt-link key="Pricing" to="#price" class="block py-2 pr-4 pl-3 text-base lg:text-2xl font-medium text-white hover:text-indigo-50">
-              Tarifs
-            </nuxt-link>
-
-            <nuxt-link key="Services" to="#services" class="block py-2 pr-4 pl-3 text-base lg:text-2xl font-medium text-white hover:text-indigo-50">
-              Services
-            </nuxt-link>
-
-            <nuxt-link key="Company" to="#escape" class="block py-2 pr-4 pl-3 text-base lg:text-2xl font-medium text-white hover:text-indigo-50">
-              Escape
-            </nuxt-link>
-
-            <nuxt-link key="Arcade" to="#arcade" class="block py-2 pr-4 pl-3 text-base lg:text-2xl font-medium text-white hover:text-indigo-50">
-              Arcade
-            </nuxt-link>
-
-            <nuxt-link key="Docs" to="#culture" class="block py-2 pr-4 pl-3 text-base lg:text-2xl font-medium text-white hover:text-indigo-50">
-              Culture
-            </nuxt-link>
-          </div>
+            Accueil
+          </nuxt-link>
+          <!-- tarifs -->
+          <nuxt-link
+            key="Pricing"
+            to="#price"
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
+          >
+            Tarifs
+          </nuxt-link>
+          <!-- escape -->
+          <nuxt-link
+            key="Company"
+            to="#escape"
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
+          >
+            Escape
+          </nuxt-link>
+          <!-- arcade -->
+          <nuxt-link
+            key="Docs"
+            to="#arcade"
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
+          >
+            Arcade
+          </nuxt-link>
+          <!-- culture -->
+          <nuxt-link
+            key="Docs"
+            to="#culture"
+            class="text-base lg:text-2xl font-medium text-white hover:text-indigo-50"
+          >
+            Culture
+          </nuxt-link>
         </div>
       </div>
     </nav>
@@ -75,16 +81,6 @@ export default {
       type: Object,
       required: true,
       default: null
-    }
-  },
-  data () {
-    return {
-      showMenu: false
-    }
-  },
-  methods: {
-    toggleNavbar () {
-      this.showMenu = !this.showMenu
     }
   }
 }
