175e7dbfd1869e55f27b68d439038d0af61cc497
sandro <sandroazerty@icloud.com>
2022-08-28 19:42:32 +0200

    fix funcking errors of enzo

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <Header :logo="Logo" />
+    <pre>
+      {{ Synthesys.filter(el=>el.imageUrl === null) }}
+    </pre>
+    <!-- <Header :logo="Logo" />
     <nuxt-img
       provider="sanity"
       class="text-center"
       :src="LandingImage.imageId.asset._ref"
       alt="VR headset"
       sizes="xs:100vw lg:100vw xl:100vw"
-    />
-    <Tarif />
-    <Services :pages="pages" />
+    /> -->
+    <!-- <Tarif /> -->
+    <!-- <Services :pages="pages" /> -->
     <Titles title="ESCAPE GAME VR" subtitle="UBISOFT" />
     <Card :games="Ubisoft" />
     <Titles title="ESCAPE GAME VR (EXCLU)" subtitle="ARVI VR" />
@@ -19,35 +22,30 @@
     <Titles subtitle="SYNTHESYS" />
     <Card :games="Synthesys" />
     <Titles subtitle="PARTENAIRES" />
-    <Card :games="EditorsResult" :editor="false" />
-    <Footer />
+    <!-- <Card :games="EditorsResult" :editor="false" /> -->
+    <!-- <Footer /> -->
   </div>
 </template>
 
 <script>
-import Titles from '~/components/Titles.vue'
 export default {
   name: 'IndexPage',
-  components: { Titles },
-  data () {
-    return {
-      games: null,
-      pages: null,
-      editors: null
-    }
+  async asyncData ({ $axios }) {
+    const games = await $axios.$get('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "game"]{name,text,"imageUrl": image{asset},editor->{name},slug{current}}')
+    return { games }
   },
-  async fetch () {
-    this.games = await fetch('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "game"]{name,text,"imageUrl": image{asset},editor->{name},slug{current}}').then(res => res.json())
-    this.pages = await fetch('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "Pages"]{name,"imageId":image{asset},titre,text}').then(res => res.json())
-    this.editors = await fetch('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "editors"]{name,"imageUrl":image{asset},_id,slug}').then(res => res.json())
-  },
+  // async fetch () {
+  //   this.pages = await fetch('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "Pages"]{name,"imageId":image{asset},titre,text}').then(res => res.json())
+  //   this.editors = await fetch('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "editors"]{name,"imageUrl":image{asset},_id,slug}').then(res => res.json())
+  // },
   computed: {
-    Logo () {
-      return this.pages.result.find(el => el.name === 'Logo')
-    },
-    LandingImage () {
-      return this.pages.result.find(el => el.name === 'Landing Image')
-    },
+
+    //   Logo () {
+    //     return this.pages.result.find(el => el.name === 'Logo')
+    //   },
+    //   LandingImage () {
+    //     return this.pages.result.find(el => el.name === 'Landing Image')
+    //   },
     Ubisoft () {
       return this.games.result.filter(el => el.editor.name === 'Ubisoft')
     },
@@ -59,10 +57,10 @@ export default {
     },
     Synthesys () {
       return this.games.result.filter(el => el.editor.name === 'Synthesys')
-    },
-    EditorsResult () {
-      return this.editors.result
     }
+  //   EditorsResult () {
+  //     return this.editors.result
+  //   }
   }
 }
 </script>
