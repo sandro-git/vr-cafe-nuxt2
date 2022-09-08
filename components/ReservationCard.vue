32cb346b7da397c84d156328afee28db39f33e9b
sandro <sandroazerty@icloud.com>
2022-09-08 08:41:32 +0200

    add reservation card and connect to store and sanity

---
+++ components/ReservationCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="grid grid-cols-1 grid-row-3 place-self-auto xl:grid-cols-4 gap-4 items-center p-10  ">
+    <div v-for="service in services" :key="service._id" class="flex flex-col items-center h-full w-full  rounded-xl ">
+      <nuxt-link :to="service.slug.current" class="max-w-sm overflow-hidden rounded-xl shadow-md duration-200 hover:scale-105 hover:shadow-xl w-full h-full border-2 border-pink-500">
+        <div>
+          <nuxt-img
+            provider="sanity"
+            :src="service.imageUrl.asset._ref"
+            placeholder
+            class="h-52 w-full"
+          />
+          <div class="p-5">
+            <p class="text-2xl text-center mb-5 text-gray-50">
+              {{ service.name }}
+            </p>
+            <button class="w-full rounded-md bg-indigo-700  py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">
+              Choisir
+            </button>
+          </div>
+        </div>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    services: {
+      default: null,
+      type: Array
+    }
+  }
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -1,7 +1,8 @@
 export const state = () => ({
   games: [],
   pages: [],
-  editors: []
+  editors: [],
+  services: []
 })
 
 export const getters = {
@@ -13,6 +14,9 @@ export const getters = {
   },
   getEditors (state) {
     return state.editors
+  },
+  getServices (state) {
+    return state.services
   }
 }
 
@@ -25,6 +29,9 @@ export const mutations = {
   },
   SET_EDITORS (state, editors) {
     state.editors = editors
+  },
+  SET_SERVICES (state, services) {
+    state.services = services
   }
 }
 
@@ -36,9 +43,12 @@ export const actions = {
 
     const editors = await $axios.$get('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "editors"]{name,"imageUrl":image{asset},_id,link}')
 
+    const services = await $axios.$get('https://byaeh17d.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == "services"]{name,"imageUrl":image{asset},_id,slug}')
+
     await dispatch('setGames', games.result)
     await dispatch('setPages', pages.result)
     await dispatch('setEditors', editors.result)
+    await dispatch('setServices', services.result)
   },
   setGames ({ commit }, games) {
     commit('SET_GAMES', games)
@@ -48,5 +58,8 @@ export const actions = {
   },
   setEditors ({ commit }, editors) {
     commit('SET_EDITORS', editors)
+  },
+  setServices ({ commit }, services) {
+    commit('SET_SERVICES', services)
   }
 }
