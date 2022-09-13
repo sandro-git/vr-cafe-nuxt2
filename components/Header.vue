2cd3a30f07c5e578191d54e4620a38b9b0d72138
sandro <sandroazerty@icloud.com>
2022-09-13 19:08:02 +0200

    try adding simplybookme widget

--- components/Header.vue
+++ components/Header.vue
@@ -19,12 +19,18 @@
         <i class="fas fa-bars h-8 w-8" />
       </div>
       <div class=" hidden lg:flex justify-center lg:col-start-3">
-        <nuxt-link
+        <!-- <nuxt-link
           to="/reservation"
           class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base lg:text-2xl font-medium text-indigo-700 hover:bg-indigo-50"
         >
           Réserver
-        </nuxt-link>
+        </nuxt-link> -->
+        <a
+          href="/reservation.html"
+          class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base lg:text-2xl font-medium text-indigo-700 hover:bg-indigo-50"
+        >
+          Réserver
+        </a>
       </div>
       <!-- navigation -->
       <div
