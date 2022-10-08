d005d3019605b13210fe5af2b302d9031c445763
sandro das neves <sandroazerty@icloud.com>
2022-10-08 15:23:29 +0200

    add adress

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -1,6 +1,31 @@
 <template>
-  <div id="contact" class="bg-gray-900 flex justify-center space-x-32 mt-12">
-    <div>
+  <div id="contact" class="bg-gray-900 flex flex-col justify-center mt-12">
+    <div class="text-center">
+      <h3 class="text-xl  font-medium leading-6 text-gray-50">
+        Adresse
+      </h3>
+      <dl class="mt-2 text-lg text-gray-200">
+        <div>
+          <dt class="sr-only">
+            Adresse
+          </dt>
+          <a
+            href="https://www.google.com/maps/place/VR+Caf%C3%A9+PERPIGNAN+le+meilleur+de+la+r%C3%A9alite+virtuelle/@42.7070353,3.0186066,15z/data=!4m12!1m6!3m5!1s0x0:0x21ddaea797d553b8!2sVR+Caf%C3%A9+PERPIGNAN+le+meilleur+de+la+r%C3%A9alite+virtuelle!8m2!3d42.7070353!4d3.0186066!3m4!1s0x0:0x21ddaea797d553b8!8m2!3d42.7070353!4d3.0186066">
+            <dd>2 rue du pilou</dd>
+          </a>
+        </div>
+        <div class="mt-1">
+          <dt class="sr-only">
+            Code postal et ville
+          </dt>
+          <a
+            href="https://www.google.com/maps/place/VR+Caf%C3%A9+PERPIGNAN+le+meilleur+de+la+r%C3%A9alite+virtuelle/@42.7070353,3.0186066,15z/data=!4m12!1m6!3m5!1s0x0:0x21ddaea797d553b8!2sVR+Caf%C3%A9+PERPIGNAN+le+meilleur+de+la+r%C3%A9alite+virtuelle!8m2!3d42.7070353!4d3.0186066!3m4!1s0x0:0x21ddaea797d553b8!8m2!3d42.7070353!4d3.0186066">
+            <dd>66140 - Canet En roussillon</dd>
+          </a>
+        </div>
+      </dl>
+    </div>
+    <div class="text-center mt-8">
       <h3 class="text-xl font-medium leading-6 text-gray-50">
         Sandro
       </h3>
@@ -23,7 +48,7 @@
         </div>
       </dl>
     </div>
-    <div>
+    <div class="text-center mt-8">
       <h3 class="text-xl font-medium leading-6 text-gray-50">
         Contact
       </h3>
