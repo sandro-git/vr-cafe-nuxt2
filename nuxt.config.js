bd75b70edf63b65609818a27a5af8b49bf375ded
sandro-git <sandroazerty@icloud.com>
2023-04-03 23:19:48 +0200

    updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,61 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'VR Café',
+    title: "VR Café",
     htmlAttrs: {
-      lang: 'fr'
+      lang: "fr",
     },
     bodyAttrs: {
-      class: 'bg-gray-900 text-gray-50'
+      class: "bg-gray-900 text-gray-50",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'La page web de la plus grande salle de réalité virtuelle des pyrénée-orientales. Vous pouvez retouvez tout nos Jeux. Et surtout réserver votre session.' },
-      { name: 'format-detection', content: 'telephone=no' },
-      { name: 'msapplication-TileColor', content: '#000000' },
-      { name: 'theme-color', content: '#000000' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "La page web de la plus grande salle de réalité virtuelle des pyrénée-orientales. Vous pouvez retouvez tout nos Jeux. Et surtout réserver votre session.",
+      },
+      { name: "format-detection", content: "telephone=no" },
+      { name: "msapplication-TileColor", content: "#000000" },
+      { name: "theme-color", content: "#000000" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5b8dd5' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/site.webmanifest" },
+      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5b8dd5" },
     ],
-    script: [
-      { src: '//widget.simplybook.it/v2/widget/widget.js' }
-    ]
+    script: [{ src: "//widget.simplybook.it/v2/widget/widget.js" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     // font-awesome
-    '@plugins/font-awesome'
+    "@plugins/font-awesome",
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -48,30 +64,29 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
     // nuxt-image
-    '@nuxt/image',
+    "@nuxt/image",
     // nuxt-sanity
-    '@nuxtjs/sanity/module',
-    '@nuxtjs/google-analytics'
+    "@nuxtjs/sanity/module",
+    "@nuxtjs/google-analytics",
   ],
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
+    baseURL: "/",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
   // sanity: {
   //   projectId: 'byaeh17d',
   //   minimal: true
@@ -79,19 +94,20 @@ export default {
   tailwindcss: {},
   image: {
     sanity: {
-      projectId: 'byaeh17d'
-    }
+      projectId: "byaeh17d",
+    },
   },
   sanity: {
-    projectId: 'byaeh17d'
+    projectId: "byaeh17d",
+    minimal: true,
   },
   googleAnalytics: {
-    id: process.env.GOOGLE_ANALYTICS_ID
+    id: process.env.GOOGLE_ANALYTICS_ID,
     // Use as fallback if no runtime config is provided
   },
   publicRuntimeConfig: {
     googleAnalytics: {
-      id: process.env.GOOGLE_ANALYTICS_ID
-    }
-  }
-}
+      id: process.env.GOOGLE_ANALYTICS_ID,
+    },
+  },
+};
