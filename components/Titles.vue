6855f30361aed86961a0bb5e09f27bba99728be4
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2023-11-16 11:01:48 +0100

    change title style

--- components/Titles.vue
+++ components/Titles.vue
@@ -1,6 +1,6 @@
 <template>
   <div :id="id" class="bg-gray-900 grid grid-cols-1 grid-rows-2 place-items-center">
-    <h2 class="sm:text-4xl lg:text-5xl text-3xl font-extrabold text-white text-center px-4 py-4 mt-4  border-pink-500 border-2 w-1/2 ">
+    <h2 v-if="title" class="sm:text-4xl lg:text-5xl text-3xl font-extrabold text-white text-center px-4 py-4 mt-4  border-pink-500 border-2 w-1/2 ">
       <span class=" ">{{ title }}</span>
     </h2>
     <div v-if="subtitle" class="flex w-full justify-center items-center bg-gray-800 py-8">
